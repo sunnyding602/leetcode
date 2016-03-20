ec46b493906ad13bba93c058516f5557b033a66d
Sunny <sunnyding602@gmail.com>
2016-03-19 23:23:16 -0400

    Create 293. Flip Game.java

---
+++ 293. Flip Game.java
@@ -0,0 +1,12 @@
+//o(n)
+public class Solution {
+    public List<String> generatePossibleNextMoves(String s) {
+        List<String> list = new ArrayList<>();
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i-1) == '+' && s.charAt(i) == '+'){
+                list.add( s.substring(0, i-1) +"--" + s.substring(i+1,s.length() ));
+            }
+        }
+        return list;
+    }
+}
