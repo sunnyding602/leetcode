b5ecd4ee2426776ea8c4550579b42e01faa51c89
Sunny <sunnyding602@gmail.com>
2016-03-23 18:14:05 -0400

    Create 136. Single Number.java

---
+++ 136. Single Number.java
@@ -0,0 +1,10 @@
+//o(n)
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int n : nums){
+            res = res ^ n;
+        }
+        return res;
+    }
+}
